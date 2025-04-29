56405975206daae9771a8a3780cd653360f7394c
Sivaramalingam Kirushanth <sivaramalingamk@vau.ac.lk>
2025-04-29 06:55:35 +0000

    runtime args

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -1,1 +1,19 @@
+class ExceptionDemo{
+    public static void main(String[] args){
+        System.out.println("HI");
+        
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        try{
+            System.out.println("a ="+a+" , b ="+b);
+            System.out.println("a/b is "+ a/b);
+        }catch(Exception e){
+            System.out.println("Divide by 0 ");
+        }
+        
+        System.out.println("Done!");
+        
 
+
+    }
+}
