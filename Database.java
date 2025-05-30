fc5adf41d917ab011a33a85b0c6142f0e86ab31f
nethsara-rahiru <rahiru123@gmail.com>
2025-05-30 11:50:13 +0530

    Updated the Switch

--- Database.java
+++ Database.java
@@ -24,6 +24,54 @@ public class Database{
         do{
             printMenue();
             option=sc.nextInt();
+      			switch(option){
+      				case 1:
+      					System.out.print("Please enter Item Name           :");
+      					String name = scan.nextLine();
+      					
+      					System.out.print("Please enter Description         :");
+      					String description = scan.nextLine();
+      					
+      					System.out.print("Please enter Unit Price          :");
+      					int Price = Integer.parseInt(scan.nextLine());
+      					
+      					System.out.print("Please enter Quantity Available  :");
+      					int Quantity = Integer.parseInt(scan.nextLine());
+      					
+      					Item item = new Item(name, description, Price, Quantity);
+      					db.addItem(item);
+      					break;
+      					
+      				case 2:
+      					System.out.print("Please enter Item Name to remove");
+      					String name = scan.nextLine();
+      					db.removeItem(name);
+      					break;
+
+              case 3:
+      					//Add method here
+      					break;
+
+              case 4:
+      					//Add method here
+      					break;
+                
+      				case 5:
+      					//Add method here
+      					break;
+
+              case 6:
+      					//Add method here
+      					break;
+
+              case 7:
+                //Add method here
+      					break;
+                
+      				case 0:
+      					system = 0;
+      					break;
+      			}
         }while(option!=0);
 
     }rhyruyu
